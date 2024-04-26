1349d1dcba3d39326dc2c4638ffc1e500f6a9296
dawitmesfin12 <dawitquanta@gmail.com>
2024-04-26 11:16:28 +0300

    undersanding curring in javascript

---
+++ Curring.js
@@ -0,0 +1,20 @@
+
+const adder = (a) =>{
+   return (b) =>{
+       return (c) => {
+           return a + b + c
+       }
+   }
+}
+
+console.log(adder(3)(4)(5)) // this is what curring is.
+
+// it is a proccess of transforming a function with multiple argumnet in to a sequence of functions each taking a single argument
+
+// traditionaly it would be the following with out curring
+
+const adderWithOutCurring = (a,b,c) => {
+    return a + b + c
+}
+
+// Done curring
